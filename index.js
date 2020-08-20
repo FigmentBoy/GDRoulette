327fc6f49fa703a64e93d9d0e4c55ae9299daac0
FigmentBoy <17240592+FigmentBoy@users.noreply.github.com>
2020-08-20 11:43:36 -0500

    Added creator roulette and cached pages

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ let listnum = 0
 let nextpercent = 1
 let diffstr = ""
 let custom = false
+let creator = false
+
+let pages = {}
 
 function startroulette() {
     const radios = document.getElementsByName('difficulty');
     let query = '*';
-    let addons = '&count=1';
 
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
@@ -44,15 +46,35 @@ function startroulette() {
                 
             </div>`)
         }, 250)
+    } else if(query == '*creator') {
+        creator = true
+
+        document.getElementById('settings').classList.add('animate__fadeOut')
+        setTimeout(() => {
+            document.getElementById('settings').classList.add('is-hidden')
+            document.getElementById('settings').classList.remove('is-loading')
+
+            document.getElementById('levels').insertAdjacentHTML('beforeend', 
+            `<div class='box is-centered columns animate__animated animate__fadeInUpBig mt-1' id='custombox'>
+                <div class="column box-content">
+                    <input id='customs' class='input' placeholder="Enter the creator's name"></input>
+                    <br><br>
+                    <div id="startcustom" class="button is-success" onclick="startcustom()">Start</div>
+                </div>
+                
+            </div>`)
+        }, 250)
     } else {
         document.getElementById('start').classList.add('is-loading')
 
         setTimeout(() => { // Because js is weird
-            req = new XMLHttpRequest();
-            req.open("GET", api + query + addons, false);
-            req.send(null);
+            apiquery = api + query;
+
+            req = getPage(0)
             
-            levels = JSON.parse(req.responseText)[0]["results"]
+            
+
+            levels = req[0]["results"]
 
 
             for (i = 1; i <= levels; i++) {
@@ -62,45 +84,98 @@ function startroulette() {
 
             list = list.slice(0,100)
 
-            apiquery = api + query;
+            
+
+            getNextLvl()
 
             document.getElementById('settings').classList.add('animate__fadeOut')
             setTimeout(() => {
                 document.getElementById('settings').classList.add('is-hidden')
                 document.getElementById('settings').classList.remove('is-loading')
 
-                getNextLvl()
+                
             }, 250)
         })
     }
 }
 
 function startcustom() {
-    document.getElementById('startcustom').classList.add('is-loading')
 
-    listtxt = document.getElementById('customs').value
+    if (creator) {
+        creatortxt = document.getElementById('customs').value
+        document.getElementById('startcustom').classList.add('is-loading')
 
-    list = listtxt.split('\n')
-    
-    for (i = 0; i < list.length; i++) {
-        list[i] = list[i].split(' ').join()
-    }
+        setTimeout(() => { // Because js is weird
+            req = new XMLHttpRequest();
+            req.open("GET", 'https://www.gdbrowser.com/api/profile/' + creatortxt, false);
+            req.send(null);
 
-    list.shuffle()
-    list = list.slice(0, 100)
+            creatortxt = JSON.parse(req.responseText)['accountID']
 
-    document.getElementById('custombox').classList.add('animate__fadeOut')
-    setTimeout(() => {
-        document.getElementById('custombox').classList.add('is-hidden')
-        document.getElementById('custombox').classList.remove('is-loading')
+            apiquery = api + '*?creators=' + creatortxt
+
+            levels = 0
+            amount = 10
+            page = 0
+            while (amount == 10) {
+                req = getPage(page)
+
+                amount = req.length
+                levels += amount
+                page++
+
+                console.log(amount)
+            }
+
+            for (i = 1; i <= levels; i++) { // I know this is super inefficient but the api doesn't show the amount of results for creator searches
+                list.push(i)
+            }
+
+            
+            list.shuffle()
+
+            
+
+            getNextLvl()
+
+            document.getElementById('custombox').classList.add('animate__fadeOut')
+            setTimeout(() => {
+                document.getElementById('custombox').classList.add('is-hidden')
+                document.getElementById('custombox').classList.remove('is-loading')
+
+                
+            }, 250)
+        })
+    } else {
+        document.getElementById('startcustom').classList.add('is-loading')
+
+        listtxt = document.getElementById('customs').value
+
+        list = listtxt.split('\n')
+        
+        for (i = 0; i < list.length; i++) {
+            list[i] = list[i].split(' ').join()
+        }
+
+        list.shuffle()
+        list = list.slice(0, 100)
 
         getNextLvlCustom()
-    }, 250)
+
+        document.getElementById('custombox').classList.add('animate__fadeOut')
+        setTimeout(() => {
+            document.getElementById('custombox').classList.add('is-hidden')
+            document.getElementById('custombox').classList.remove('is-loading')
+
+            
+        }, 250)
+    }
+    
 
 }
 
 function getNextLvlCustom() {
-    listnum++;
+    
 
     leveln = list.slice(0, 1)[0]
     console.log(leveln)
@@ -114,6 +189,7 @@ function getNextLvlCustom() {
     console.log(level)
     
     if (level != undefined) { // If the servers goof up
+        listnum++;
         document.getElementById('levels').insertAdjacentHTML('beforeend', 
         `<div class='box is-centered columns animate__animated animate__fadeInUpBig mt-1'>
             <div class="column box-content">
@@ -139,22 +215,19 @@ function getNextLvlCustom() {
 }
 
 function getNextLvl() {
-    listnum++;
-
     leveln = list.slice(0, 1)[0]
     console.log(leveln)
     list = list.slice(1)
 
-    page = Math.ceil(leveln/10)
+    page = Math.floor((leveln-1)/10)
 
-    req = new XMLHttpRequest();
-    req.open("GET", apiquery + '&page=' + page, false);
-    req.send(null);
+    req = getPage(page)
     
-    level = JSON.parse(req.responseText)[(leveln-1)%10]
+    level = req[(leveln-1)%10]
     console.log(level)
     
     if (level != undefined) { // If the servers goof up
+        listnum++;
         document.getElementById('levels').insertAdjacentHTML('beforeend', 
         `<div class='box is-centered columns animate__animated animate__fadeInUpBig mt-1'>
             <div class="column box-content">
@@ -182,34 +255,27 @@ function getNextLvl() {
 function showFinalLevels() {
     let element = document.getElementById('show-remaining')
     element.removeAttribute('onclick')
-    element.classList.add('is-loading')
+    if (!creator) element.classList.add('is-loading')
     
     str = ''
-    pages = {}
 
     setTimeout(() => { // Idk why but it doesn't set the loading thing correctly unless I do this.
         for (leveln of list) {
-            listnum++;
+            
             nextpercent++;
             
             if (nextpercent > 100) {
                 break
             }
     
-            page = Math.ceil(leveln/10)
+            page = Math.floor((leveln-1)/10)
     
-            if (!pages.hasOwnProperty(page)) {
-                req = new XMLHttpRequest();
-                req.open("GET", apiquery + '&page=' + page, false);
-                req.send(null);
+            page = getPage(page)
     
-                pages[page] = JSON.parse(req.responseText)
-            }
-    
-            level = pages[page][(leveln-1)%10]
-            console.log(leveln)
+            level = page[(leveln-1)%10]
             
             if (level != undefined) { // If the servers goof up
+                listnum++;
                 str += `<div class='box is-centered columns animate__animated animate__fadeInUpBig mt-1'>
                     <div class="column box-content">
                         <h1 class="title">#${listnum}: ${level.name}</h1>
@@ -219,6 +285,8 @@ function showFinalLevels() {
                         ${nextpercent}%
                     </div>
                 </div>`
+            } else {
+                console.log(leveln)
             }
         }
 
@@ -230,14 +298,14 @@ function showFinalLevels() {
 function showFinalLevelsCustom() {
     let element = document.getElementById('show-remaining')
     element.removeAttribute('onclick')
-    element.classList.add('is-loading')
+    if (!creator) element.classList.add('is-loading')
     
     str = ''
     pages = {}
 
     setTimeout(() => { // Idk why but it doesn't set the loading thing correctly unless I do this.
         for (leveln of list) {
-            listnum++;
+            
             nextpercent++;
             
             if (nextpercent > 100) {
@@ -251,6 +319,7 @@ function showFinalLevelsCustom() {
             level = JSON.parse(req.responseText)
 
             if (level != undefined) { // If the servers goof up
+                listnum++;
                 str += `<div class='box is-centered columns animate__animated animate__fadeInUpBig mt-1'>
                     <div class="column box-content">
                         <h1 class="title">#${listnum}: ${level.name}</h1>
@@ -270,30 +339,37 @@ function showFinalLevelsCustom() {
 
 
 function complete() {
-    percent = parseInt(document.getElementById('percent').value)
-    if (percent >= nextpercent && percent < 100 && list.length > 0) {
-        nextpercent = percent + 1
+    if (!creator) document.getElementById('completion').classList.add('is-loading')
+    setTimeout(() => {
+        percent = parseInt(document.getElementById('percent').value)
+        console.log(percent >= nextpercent && percent < 100 && list.length > 0)
 
-        old = document.getElementById('temp')
-        old.id = ''
-        old.innerHTML = `${percent}%`
+        if (percent < nextpercent || percent > 100) {
+            alert('Nice try lmao')
+        } else if (percent >= nextpercent && percent < 100 && list.length > 0) {
+            nextpercent = percent + 1
 
-        if (custom) {
-            getNextLvlCustom()
-        } else {
-            getNextLvl()
-        }
-        
-    } else if (percent == 100 || list.length == 0) {
-        nextpercent = percent + 1
-        listnum++;
-        old = document.getElementById('temp')
-        old.id = ''
-        old.innerHTML = `${percent}%`
-        finish()
-    } else {
-        alert('Nice try lmao')
-    }
+            old = document.getElementById('temp')
+            old.id = ''
+            old.innerHTML = `${percent}%`
+
+            if (custom) {
+                getNextLvlCustom()
+            } else {
+                getNextLvl()
+            }
+        } else if (percent == 100 || list.length == 0) {
+            list.splice(0, 1)
+            nextpercent = percent + 1
+            listnum++;
+            old = document.getElementById('temp')
+            old.id = ''
+            old.innerHTML = `${percent}%`
+    
+            finish()
+        } 
+    })
+    
 }
 
 function finish() {
@@ -336,6 +412,19 @@ function copy(string) {
     copyText.select();
     copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
+}
+
+function getPage(page) {
+
+    if (!pages.hasOwnProperty(`page-${page}`)) {
+        req = new XMLHttpRequest();
+        req.open("GET", apiquery + '&page=' + page, false);
+        req.send(null);
+
+        pages[`page-${page}`] = JSON.parse(req.responseText)
+    }
+
+    return (pages[`page-${page}`])
 }
 
 // https://stackoverflow.com/a/6274381/9124836
